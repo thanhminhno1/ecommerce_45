ed1fd16593c6f1a0de8e3df2d67d1200f3abedab
Minh <nguyenthanhminh10101994@gmail.com>
2018-05-11 08:12:18 +0700

    test

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -4,23 +4,4 @@ $(document).ready(function() {
   $('#myCarousel').carousel()
   $('#newProductCar').carousel()
 
-/* Home page item price animation */
-  $('.thumbnail').mouseenter(function() {
-    $(this).children('.zoomTool').fadeIn();
-  });
-
-  $('.thumbnail').mouseleave(function() {
-  	$(this).children('.zoomTool').fadeOut();
-  });
-
-// Show/Hide Sticky 'Go to top' button
-	$(window).scroll(function(){
-		if($(this).scrollTop()>200){
-			$('.gotop').fadeIn(200);
-		}
-		else{
-			$('.gotop').fadeOut(200);
-		}
-	});
-
 });
